ec2f4030a11c93c0586b4933b39f37843088cbdc
Евгения Белоярцева <evgeniya.kornako@mail.ru>
2021-01-24 18:23:40 +0300

    3

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const lev = document.querySelectorAll('.level__choice');
 const game = document.querySelectorAll('.button');
 const games = document.querySelectorAll('.game__one');
 const container = document.querySelector('.game');
+let gameTable = document.querySelectorAll('.game_cards');
 let numberCards = 3;
+
 
 
 lev.forEach(function(element) {
@@ -29,27 +31,46 @@ lev.forEach(function(element) {
     })  
 });
 
-
+function random() {
+	return Math.round(Math.random());
+}
 
 createCard = () => {
    let newCards = document.createElement('div');
-   newCards.classList = 'card_back';
+   newCards.classList = 'card_back card';
    container.append(newCards); 
+   switch (numberCards) {
+       case 10:
+            container.classList.toggle("level-hard");
+           break;
+    }
+   newCards.addEventListener("click", function() {
+      random () ===0 ? newCards.classList.add('card_front') : newCards.classList.add('card_bag')
+   		newCards.classList.remove('card_back');
+   		let block = document.createElement('div');
+   		block.classList = 'block';
+   		document.body.append(block);
+   		block.addEventListener("click", () => {
+   			games[0].classList.remove('field');
+   			container.classList.remove('level-hard');
+   			// container.classList.add('field');
+   			block.remove();
+   			let cards = document.querySelectorAll('.card');
+   			cards.forEach(el => el.remove())
+   		})
+   });
  }
 
 game.forEach(function(event) {
   event.classList.remove('field');
   event.addEventListener("click", function() {
+  	  container.classList.remove('field');
       games.forEach(function (event) {
       event.classList.add('field');
     })
+
     for (let i = 0; i < numberCards; i++ ) {
        createCard()
       }
     })
   });
-
-
-
-
-
